4ddc53849469b119877168dddf9449a7ad20a98f
MasLiang <liangjiaweibuaa@163.com>
2024-08-24 14:51:57 +0800

    change the cg_insertion from DFS to BFS with Power saved

--- insert/src/cg_insert.py
+++ insert/src/cg_insert.py
@@ -33,7 +33,7 @@ def determain_cgen(top_module_ast, undf_flg):
             
         if ap_idle_flg:
             cgen = "!("+cgen_n+" | ap_idle)"
-        else
+        else:
             cgen = "!("+cgen_n+")"
         return cgen
     else:
@@ -214,36 +214,104 @@ def cg_inst_gen(inst_name, cgen):
             f.write(i)
             f.write("\n")
 
-def cg_insert(module_name, root_path, cg_num, cg_max_num, cg_level, cg_max_level, top_module=1):
-    if os.path.exists(root_path+"/"+module_name+".v"):
-        top_module_ast, _, cg_module_list, main_module_list, _, _, other_module_list, _, case_always_list, _, _, _ = read_file(module_name, {}, root_path)
+def rpt_parser(module_name, rpt_root_path, pre_latency):
+    # TODO: this should be input as device info
+    power_table = {"BRAM_18K": 1, "DSP": 1, "FF": 1, "LUT" : 1, "URAM" : 1}
+    rpt_path = rpt_root_path+"/"+module_name+"_csynth.rpt"
 
-        # if there is not a clock gate
-        if len(cg_module_list)==0:
-            undf_flg = 0 # 1: this is a un-dataflow module with FSM
-            if len(case_always_list)>0:
-                for case_always in case_always_list:
-                    if case_always.statement.statements[0].comp.name=="ap_CS_fsm":
-                        undf_flg = 1
-                        #cg_insert_single_module(module_name, root_path, undf_flg, top_module)
-                        cg_insert_single_module(module_name, top_module_ast, top_module, root_path)
-                        cg_num += 1
-                        break
-        cg_level += 1
+    with open(rpt_path, 'r') as file:
+        lines = file.readlines()
+    
+    latency_flg = 0
+    resrouce_type_flg = 0
+    
 
-        if cg_level==cg_max_level or cg_num==cg_max_num:
-            return cg_num
+    latency_head_pattern = r'\s*\|\s*Latency \(cycles\)\s*\|\s*Latency \(absolute\)\s*\|\s*Interval\s*\|\s*Pipeline\s*\|'
+    resource_head_pattern = '== Utilization Estimates'
+    for line in lines:
+        if not latency_flg:
+            match = re.match(latency_head_pattern, line)
+            if match:
+                latency_flg = 1
+                latency_head_line = line
+                latency_line_idx = lines.index(latency_head_line) + 3
+                latency_line = lines[latency_line_idx].replace(" ", "").split("|")
+                latency = latency_line[1]
+        elif not resource_type_flg:
+            match = re.match(resource_head_pattern, line)
+            if match:
+                resource_type_flg = 1
+                resource_head_line = line
+                resource_type_line_idx = lines.index(resource_head_line, line) + 4
+                resource_type = lines[latency_line_idx].replace(" ", "").split("|")[2:]
+        else:
+            match = re.match("|Total", line)
+            if match:
+                resource_num = line.replace(" ", "").splie("|")[2:]
+                break
+
+    
+        power = 0
+        for resource_idx in range(len(resource_type)):
+            power += power_table[resource_type[resource_idx]] * int(resource_num[resource_idx]) 
+    
+        if pre_latency==0:
+            return latency, 0
+        power_saved = power * (1- latency / pre_latency)
         
-        for inst in main_module_list+other_module_list:
-            # Why not this file?
-            if not os.path.exists(root_path+"/"+inst.module+".v"):
-                continue
-            # read and parse this file
-            cg_num = cg_insert(inst.module, root_path, cg_num, cg_max_num, cg_level, cg_max_level, top_module=0)
-            if cg_num==cg_max_num:
-                return cg_num
+        return latency, power_saved
 
+def cg_insert(module_name, root_path, rpt_root_path, cg_max_num, cg_max_level, top_module=1):
 
-        return cg_num
+    module_list = [[], []]
+    pi_flg = 0
+    po_flg = 1
+    cg_num = 0
+    level_num = 0
+
+    tmp_latency, tmp_power_saved = rpt_parser(module_name, rpt_root_path, 0)
+    # name, latency, power_saved
+    module_list[pi_flg].append([module_name, tmp_latency, tmp_power_saved])
+
+    while 1:
+        curr_module_list = module_list[pi_flg]
+        nxt_module_list = module_list[po_flg]
+        curr_module_num = len(curr_module_list)
+        nxt_module_num = len(nxt_module_list)
+        if curr_module_num==0 and nxt_module_num==0:
+            break
+
+        # sort
+        curr_module_list.sort(key=lambda md : md[2])
+        for _ in range(curr_module_num):
+            curr_module = curr_module_list.pop(0)
+            if os.path.exists(root_path+"/"+curr_module[0]+".v"):
+                top_module_ast, _, cg_module_list, main_module_list, _, _, other_module_list, _, case_always_list, _, _, _ = read_file(curr_module, {}, root_path)
+
+            # if there is not a clock gate
+            # add a gate to this module
+            if len(cg_module_list)==0:
+                undf_flg = 0 # 1: this is a un-dataflow module with FSM
+                if len(case_always_list)>0:
+                    for case_always in case_always_list:
+                        if case_always.statement.statements[0].comp.name=="ap_CS_fsm":
+                            undf_flg = 1
+                            cg_insert_single_module(module_name, top_module_ast, top_module, root_path)
+                            cg_num += 1
+                            break
             
+            if cg_num==cg_max_num:
+                return 
+
+            for inst in main_module_list+other_module_list:
+                tmp_latency, tmp_power_saved = rpt_parser(inst.module, rpt_root_path, curr_module[1])
+                nxt_module_list.append([inst.module, tmp_latency, tmp_power_saved])
+    
+        cg_level += 1
+        if cg_level==cg_max_level or cg_num==cg_max_num:
+            return 
+        tmp_flg = pi_flg
+        pi_flg = po_flg
+        po_flg = tmp_flg
+        
 #cg_insert("top_kernel3_x0", "../../all_ab/dut/solution1/impl/verilog/")
